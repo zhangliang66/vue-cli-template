88bf9736d466ada81ff285a0b03fbb6fc6453476
张亮 <787369171@qq.com>
2019-11-02 18:56:30 +0800

    修改vuex配置1

--- meta.js
+++ meta.js
@@ -175,7 +175,7 @@ module.exports = {
     'test/unit/setup.js': "unit && runner === 'jest'",
     'test/e2e/**/*': 'e2e',
     'src/router/**/*': 'router',
-    'src/store/**/*': 'router',
+    'src/store/**/*': 'vuex',
   },
   complete: function(data, { chalk }) {
     const green = chalk.green
